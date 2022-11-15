c5af22f21352ee93ecbf4194e2af78a9bb97088b
BIGOSCAR85 <rojas.osc85@gmail.com>
2022-11-15 19:46:52 +0000

    proyecto finalizado

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <div className="container-fluid bg-dark text-center py-3 text-white">
+            <p>Copyright © Your Webpage 2022</p>
+        </div>
+    )
+}
